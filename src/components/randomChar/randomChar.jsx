0fefd289da536139cde2a13a19ead0cfde5b0624
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-01 00:19:54 +0500

    add `loadChar` & `updateChar` via Api & state

--- src/components/randomChar/randomChar.jsx
+++ src/components/randomChar/randomChar.jsx
@@ -1,27 +1,60 @@
 import React, { Component } from 'react'
+import Api from '../../services/api'
 import './randomChar.css'
 
 export default class RandomChar extends Component {
+  constructor() {
+    super()
+    this.updateChar()
+  }
+  api = new Api()
+
+  state = {
+    char: {
+      name: 'Unknown',
+      gender: 'Unknown',
+      born: 'Unknown',
+      died: 'Unknown',
+      culture: 'Unknown',
+    },
+  }
+
+  loadCharHandler = char => {
+    this.setState({ char })
+  }
+
+  updateChar() {
+    const id = Math.floor(Math.random() * 1200)
+
+    this.api
+      .getCharacter(id)
+      .then(this.loadCharHandler)
+      .catch(err => console.error(err))
+      .finally(console.log(id))
+  }
+
   render() {
+    const { name, gender, born, died, culture } = this.state.char
+
     return (
       <div className="random-block rounded">
-        <h4>Random Character: John</h4>
+        <h4 className="random-block-title">Random Character: {name}</h4>
         <ul className="list-group list-group-flush">
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Gender </span>
-            <span>male</span>
+            <span>{gender || 'Unknown'}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Born </span>
-            <span>11.03.1039</span>
+            <span>{born || 'Unknown'}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Died </span>
-            <span>13.09.1089</span>
+            <span>{died || 'Unknown'}</span>
           </li>
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Culture </span>
-            <span>Anarchy</span>
+            <span>{culture || 'Unknown'}</span>
           </li>
         </ul>
       </div>
