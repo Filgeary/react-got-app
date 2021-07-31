239b3d5de2cdf9122b7b66a4bd0de15acb7e0a01
Georgiy Filippov <g.filippov777@gmail.com>
2021-07-31 15:39:17 +0500

    init commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Col, Row, Container } from 'reactstrap'
+
+// components
+import Header from '../header/header'
+import RandomChar from '../randomChar/randomChar'
+import ItemList from '../itemList/itemList'
+import CharDetails from '../charDetails/charDetails'
+
+const App = () => {
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={{ size: 5, offset: 0 }}>
+            <RandomChar />
+          </Col>
+        </Row>
+        <Row>
+          <Col md="6">
+            <ItemList />
+          </Col>
+          <Col md="6">
+            <CharDetails />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default App
