e33b8e6b5420aa8b3a5eb22aff4667020b98bc87
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-02 17:25:30 +0500

    add title & styles

--- src/components/itemList/itemList.jsx
+++ src/components/itemList/itemList.jsx
@@ -4,11 +4,20 @@ import './itemList.css'
 export default class ItemList extends Component {
   render() {
     return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">John Snow</li>
-        <li className="list-group-item">Brandon Stark</li>
-        <li className="list-group-item">Geremy</li>
-      </ul>
+      <>
+        <h3 className="item-list-title">Pick a Hero</h3>
+        <ul className="item-list list-group">
+          <li className="list-group-item" tabIndex="0">
+            John Snow
+          </li>
+          <li className="list-group-item" tabIndex="0">
+            Brandon Stark
+          </li>
+          <li className="list-group-item" tabIndex="0">
+            Geremy
+          </li>
+        </ul>
+      </>
     )
   }
 }
