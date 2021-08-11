257821141cef9914ef55c7e311ab58745952133f
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-11 18:00:16 +0500

    refactor - replace styled-components to pure CSS

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,61 +1,34 @@
 import React from 'react'
 import './header.css'
 import { Link, NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-
-const HeaderBlock = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 80px;
-  margin-bottom: 12px;
-`
-
-const HeaderTitle = styled.h1`
-  font-size: 24px;
-  color: #fff;
-  margin: 0;
-`
-
-const HeaderLinks = styled.ul`
-  display: flex;
-  gap: 8px;
-  margin: 0;
-  align-items: center;
-  color: #fff;
-  list-style-type: none;
-  li {
-    font-size: 18px;
-  }
-  a {
-    padding: 8px 12px;
-  }
-`
 
 const Header = () => {
   return (
-    <HeaderBlock>
-      <HeaderTitle>
-        <Link to="/">Game of Thrones DB</Link>
-      </HeaderTitle>
-      <HeaderLinks>
-        <li>
-          <NavLink to="/characters" activeClassName="header__link--selected">
-            Characters
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/houses" activeClassName="header__link--selected">
-            Houses
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/books" activeClassName="header__link--selected">
-            Books
-          </NavLink>
-        </li>
-      </HeaderLinks>
-    </HeaderBlock>
+    <header className="header">
+      <nav className="header__block">
+        <h1 className="header__title">
+          <Link to="/">Game of Thrones DB</Link>
+        </h1>
+
+        <ul className="header__list">
+          <li>
+            <NavLink to="/characters" activeClassName="header__link--selected">
+              Characters
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/houses" activeClassName="header__link--selected">
+              Houses
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/books" activeClassName="header__link--selected">
+              Books
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
